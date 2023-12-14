9a2707cf75f4eff172bbd012104356d3da0ee2dd
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-14 12:51:58 +0530

    Login Screen

--- Signup.js
+++ Signup.js
@@ -1,55 +1,71 @@
-import React, { useContext, useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import classes from './Signup.module.css';
-import AuthContext from '../pages/auth-context';
+import { Link, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../pages/auth-reducer';
 
 
-const Signup = () => {
+const Signup = (props) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
     const confirmPasswordInputRef = useRef();
+    const [isLogin, setIsLogin] = useState(true);
 
-    const Authctx = useContext(AuthContext);
-
+    const switchAuthModeHandler = () => {
+        setIsLogin((prevState) => !prevState);
+      };
     const submitHandler = (event) => {
         event.preventDefault();
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
-        const enteredConfirmPassword = confirmPasswordInputRef.current.value;
-        if (enteredPassword === enteredConfirmPassword) {
-            fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0',{
-                method: 'POST',
-                body: JSON.stringify({
-                    email: enteredEmail,
-                    password: enteredPassword,
-                    returnSecureToken: true
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-            ).then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-                else {
-                    return res.json().then(() => {
-                        let errorMessage = 'Authentication failed';
-                        throw new Error(errorMessage);
-                    })
-                }
-            }).then(data => {
-                Authctx.login(data.idToken);
-              }).catch((err) => {
-                alert(err.message);
-              })
-        }
-        else {
+        let url;
+        if (!isLogin){
+          const enteredConfirmPassword = confirmPasswordInputRef.current.value;
+          if (!isLogin && enteredPassword === enteredConfirmPassword){
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0'
+          }
+          else {
             alert('Password does not match')
+          }
         }
+        if (isLogin) {
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBSZZr78zGZyrw6I8PGUwpSUWOFci9rbm0'
+        }
+        fetch(
+        url,{
+            method: 'POST',
+            body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        }
+        ).then(res => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            return res.json().then((data) => {
+            let errorMessage = 'Authentication failed';
+            throw new Error(errorMessage);
+            });
+        }
+        }).then(data => {
+          props.email(data.localId)
+        dispatch(authActions.login(data.idToken));
+        history.replace(isLogin ?'/Home' : '/Verify');
+        }).catch((err) => {
+        alert(err.message);
+        })
+        
     }
-  return (
+    return (
     <section className={classes.auth}>
-      <h1>SignUp</h1>
+      <h1>{isLogin ? 'Login' : 'SignUp'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
@@ -64,7 +80,7 @@ const Signup = () => {
             ref={passwordInputRef}
           />
         </div>
-        <div className={classes.control}>
+        {!isLogin && <div className={classes.control}>
           <label htmlFor='password'>Confirm Password</label>
           <input
             type='password'
@@ -72,14 +88,16 @@ const Signup = () => {
             required
             ref={confirmPasswordInputRef}
           />
-        </div>
+        </div>}
         <div className={classes.actions}>
-          <button>Sign Up</button>
+        <button>{isLogin ? 'Login' : 'Sign Up'}</button>
+        {isLogin && <Link to='/ForgotPassword' >Forgot Password?</Link>}
           <button
             type='button'
             className={classes.toggle}
+            onClick={switchAuthModeHandler}
           >
-            Have an account? Login
+            {isLogin ? `Don't have account? Sign Up` : 'Have an account? Login'}
           </button>
         </div>
       </form>
