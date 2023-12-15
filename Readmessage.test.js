f0a942bc408cd2e87f949b9d27337e32db606995
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-15 20:48:31 +0530

    Read Mail TDD

---
+++ Readmessage.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Readmessage from "./Readmessage";
+
+describe('read message testing', () => {
+    test('renders p as a text', () => {
+        render(<Readmessage />)
+
+        const textElement = screen.findByRole('p');
+        expect(textElement).toBeInTheDocument();
+    })
+    test('renders div as a text', () => {
+        render(<Readmessage />)
+
+        const textElement = screen.findByRole('div');
+        expect(textElement).toBeInTheDocument();
+    })
+    test('renders h1 as a text', () => {
+        render(<Readmessage />)
+
+        const textElement = screen.findByRole('h1');
+        expect(textElement).toBeInTheDocument();
+    })
+})
