8f4c09d266c0718ee1759bb3a716eee93a082326
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2023-12-15 21:38:24 +0530

    Deleting Mail TDD

--- Readmessage.test.js
+++ Readmessage.test.js
@@ -2,22 +2,16 @@ import { render, screen } from "@testing-library/react";
 import Readmessage from "./Readmessage";
 
 describe('read message testing', () => {
-    test('renders p as a text', () => {
-        render(<Readmessage />)
+    test('render delete as a text', () => {
+        render(<Readmessage />);
 
-        const textElement = screen.findByRole('p');
+        const textElement = screen.getByText('Delete');
         expect(textElement).toBeInTheDocument();
     })
-    test('renders div as a text', () => {
-        render(<Readmessage />)
+    test('render button as a role', () => {
+        render(<Readmessage />);
 
-        const textElement = screen.findByRole('div');
-        expect(textElement).toBeInTheDocument();
-    })
-    test('renders h1 as a text', () => {
-        render(<Readmessage />)
-
-        const textElement = screen.findByRole('h1');
+        const textElement = screen.getByRole('Delete');
         expect(textElement).toBeInTheDocument();
     })
 })
